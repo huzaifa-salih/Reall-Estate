fca2be7662f6d15cdaa660b7c789e57a7928bec6
huzaifa-salih <hodaeyfha@gmail.com>
2022-06-30 01:04:10 +0300

    Full Search Text

--- backend/listing/urls.py
+++ backend/listing/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ManageListingview, ListingDetailView, ListingView
+from .views import ManageListingview, ListingDetailView, ListingView, SearchListingView
 
 
 urlpatterns = [
     path("manage/", ManageListingview.as_view()),
     path("detail/", ListingDetailView.as_view()),
     path("get-listings/", ListingView.as_view()),
-
+    path("search/", SearchListingView.as_view()),
 ]
